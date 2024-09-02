4942df31a425b7e847f848780421c83a78e86619
kwongihyeon <rrg1591rrg@naver.com>
2024-09-02 17:02:04 +0900

    중간

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import { useState } from "react";
 
 function App() {
   let [list, Setlist] = useState([]);
-  // 아 시발 문성윤 좆 같은거 시키네
-  return <div className="main"></div>;
+  let [add, Setadd] = useState("");
+
+  const onchange = (e) => {
+    Setadd(e.target.value);
+    console.log(add);
+  };
+
+  const onclick = () => {
+    let cp = [...list];
+    cp.unshift(add);
+    Setlist(cp);
+    console.log(list);
+  };
+
+  return (
+    <div className="main">
+      <div>
+        <input onChange={onchange}></input>
+        <button onClick={onclick}>+</button>
+      </div>
+      <div>{list}</div>
+    </div>
+  );
 }
 
 export default App;

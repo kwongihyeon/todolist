bfae548325926bf010313054b102b6d2f983eb44
kwongihyeon <rrg1591rrg@naver.com>
2024-09-04 20:50:13 +0900

    Mycompo에서 cgl하니까 n.text가 아니라 n.i.text로 해야 나오던데 그 이유를 모르겠어 성윤아

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import "./App.css";
 import { useState } from "react";
-import uuid from "react-uuid";
+import React from "react";
+import Mycompo from "./Mycompo";
 
 function App() {
   let [list, Setlist] = useState([]);
   let [add, Setadd] = useState("");
+  let [number, Setnumber] = useState(1);
 
   const onchange = (e) => {
     Setadd(e.target.value);
   };
 
   const onclick = () => {
-    let cp = [...list, add];
+    let cp = [...list, { id: number, text: add }];
+    Setnumber(number + 1);
     Setlist(cp);
+    Setadd("");
+  };
+
+  const ondelete = (id) => {
+    let cp2 = [...list];
+    let dt = cp2.filter((cp2) => id !== cp2.id);
+    Setlist(dt);
+  };
+
+  const onupdate = (id) => {
+    let cpcontent = [...list];
+
+    cpcontent.map((cpcontent) => {
+      if (cpcontent.id === id) {
+        cpcontent.text = add;
+      }
+    });
+    Setlist(cpcontent);
   };
 
   return (
     <div className="main">
       <div>
-        <input onChange={onchange}></input>
+        <input value={add} onChange={onchange}></input>
         <button onClick={onclick}>+</button>
       </div>
       <div>
-        {list.map((n, i) => (
-          <div key={i}>{n}</div>
+        {list.map((n) => (
+          <Mycompo n={n} ondelete={ondelete} onupdate={onupdate} />
         ))}
       </div>
     </div>
---
+++ src/Mycompo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Mycompo = (n, ondelete, onupdate) => {
+  return (
+    <div key={n.id}>
+      <span>{n.i.text}</span>
+      <button
+        onClick={() => {
+          ondelete(n.id);
+        }}
+      >
+        x
+      </button>
+      <button
+        onClick={() => {
+          onupdate(n.id);
+        }}
+      >
+        수정
+      </button>
+    </div>
+  );
+};
+
+export default Mycompo;

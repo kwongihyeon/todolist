f77dd2513309d8b605d25c7f17921ac7df94a837
kwongihyeon <rrg1591rrg@naver.com>
2024-09-09 12:56:45 +0900

    성공??

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { useState } from "react";
 import React from "react";
 import Mycompo from "./Mycompo";
 import { useEffect } from "react";
+import uuid from "react-uuid";
 
 function App() {
   let [list, Setlist] = useState([]);
   let [add, Setadd] = useState("");
   let [number, Setnumber] = useState(1);
+  let [exam, Setexam] = useState(1);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/list")
+      .then((response) => response.json())
+      .then((data) => Setlist(data));
+  }, []);
   const onchange = (e) => {
     Setadd(e.target.value);
   };
 
   const onclick = () => {
     let cp = {
-      id: number,
+      id: uuid(),
       text: add,
       switch: false,
     };
@@ -30,7 +37,6 @@ function App() {
       .then((res) => res.json())
       .then((plus) => {
         Setlist([...list, plus]);
-        console.log(list);
         Setnumber(number + 1);
       })
       .catch((error) => console.error("에러", error));
@@ -48,33 +54,33 @@ function App() {
     const sibal = list.map((n) =>
       n.id === id ? { ...n, switch: !n.switch } : n
     );
+    Setlist(sibal);
+  };
+
+  const onupdate = (id) => {
+    const bs = list.find((n) => n.id === id);
 
     fetch(`http://localhost:3000/list/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(sibal),
+      body: JSON.stringify(bs),
     })
       .then((res) => res.json())
       .then((up) => {
-        const uplist = list.map((n) => (n.id === id ? up : n));
+        const uplist = list.map((n) => (n.id === id ? { ...n, text: add } : n));
+        console.log(list);
         Setlist(uplist);
       })
       .catch((error) => console.error("수정에러", error));
   };
 
-  /*   const onupdate = (id) => {
-    let cpcontent = [...list];
-
-    cpcontent.map((cpcontent) => {
-      if (cpcontent.id === id) {
-        cpcontent.text = add;
-      }
-    });
-    Setlist(cpcontent);
+  const example = () => {
+    Setexam(exam + 1);
+    console.log(exam);
   };
- */
+
   return (
     <div className="main">
       <div className="input">
@@ -86,11 +92,12 @@ function App() {
           <Mycompo
             n={n}
             checkclick={checkclick}
-            // onupdate={onupdate}
+            onupdate={onupdate}
             key={n.id}
           />
         ))}
       </div>
+      <button onClick={example}>예제</button>
     </div>
   );
 }
